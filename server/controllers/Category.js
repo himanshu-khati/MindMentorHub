f54a5734885601f870027b0a6b74f6083b2d14d6
himanshu-khati <himanshukhati94@gmail.com>
2024-03-21 17:34:55 +0530

    rating and reviews complete

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -1,4 +1,5 @@
 const Category = require("../models/Category");
+const Course = require("../models/Course");
 // create category handler
 exports.createCategory = async (req, res) => {
   try {
@@ -44,6 +45,50 @@ exports.getAllCategory = async (req, res) => {
     return res.status(501).json({
       success: false,
       message: `something went wrong fetching category: ${error.message}`,
+    });
+  }
+};
+
+// category page details
+exports.categoryPageDetails = async (req, res) => {
+  try {
+    // get category id
+    const { categoryId } = req.body;
+    // fetch  all courses associated with specified category id
+    const selectedCategory = await Category.findById(categoryId)
+      .populate("courses")
+      .exec();
+    // validation
+    if (!selectedCategory) {
+      return res.status(404).json({
+        success: false,
+        message: `data not found`,
+      });
+    }
+    // get courses for different category
+    const differentCategories = await Category.find({
+      _id: { $ne: categoryId },
+    })
+      .populate("courses")
+      .exec();
+    // get top selling courses
+    const topSellingCourses = await Course.find()
+      .populate("studentsEnrolled")
+      .sort((a, b) => b.studentsEnrolled.length - a.studentsEnrolled.length);
+
+    // return response
+    return res.status(200).json({
+      success: true,
+      data: {
+        selectedCategory,
+        differentCategories,
+        topSellingCourses,
+      },
+    });
+  } catch (error) {
+    return res.status(201).json({
+      success: false,
+      message: `something went wrong fetching category page details: ${error.message}`,
     });
   }
 };
--- server/controllers/RatingAndReview.js
+++ server/controllers/RatingAndReview.js
@@ -107,9 +107,25 @@ exports.getAverageRating = async (req, res) => {
 };
 
 // get all rating and reviews
-/*
+
 exports.getAllRatingAndReviews = async (req, res) => {
   try {
+    const allReviews = await RatingAndReview.find({})
+      .sort({ rating: "desc" })
+      .populate({
+        path: "user",
+        select: "firstName lastName email image",
+      })
+      .populate({
+        path: "course",
+        select: "courseName",
+      })
+      .exec();
+    return res.status(200).json({
+      success: true,
+      messsage: `all reviews fetched successfully`,
+      data: allReviews,
+    });
   } catch (error) {
     return res.status(501).json({
       success: false,
@@ -117,4 +133,3 @@ exports.getAllRatingAndReviews = async (req, res) => {
     });
   }
 };
-*/
